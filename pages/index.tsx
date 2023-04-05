2a51b067c7aff88fb8eb329ba0eb4916306c7ccc
hkluck <30995928+hkluck@users.noreply.github.com>
2023-04-05 10:56:17 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -117,7 +117,7 @@ export default function Home() {
     if (hasTranslated) {
       handleTranslate();
     }
-  }, [outputLanguage]);
+  }, [outputLanguage, handleTranslate, hasTranslated]);
 
   useEffect(() => {
     const apiKey = localStorage.getItem('apiKey');
